9577d54fba381cf190d323a08d9f9e7d474f52ed
Dang Khoa <i.am@dangkhoa.dev>
2025-03-07 12:40:36 +0000

    update recipes layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,22 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    category_id = params[:category]
+    query = Recipe.all
+    if category_id.present?
+      @category = Category.find_by(id: category_id)
+      query = query.where(category_id: category_id)
+    else
+      query = query.includes(:category)
+    end
+
+    @recipes = query.to_a.sort_by(&:name)
+    count_steps_by_ids = Recipe.count_steps_by_ids(@recipes.map(&:id))
+    count_ingredients_by_ids = Recipe.count_ingredients_by_ids(@recipes.map(&:id))
+    @recipes.each do |recipe|
+      recipe.steps_count = count_steps_by_ids[recipe.id]
+      recipe.ingredients_count = count_ingredients_by_ids[recipe.id]
+    end
   end
 
   # GET /recipes/1 or /recipes/1.json
