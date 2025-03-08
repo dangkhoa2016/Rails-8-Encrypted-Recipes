4331b3bde3dcfb95be35d2c07e560ac52a791654
Dang Khoa <i.am@dangkhoa.dev>
2025-03-08 09:25:41 +0000

    add sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+  def nav_list
+    [
+      {
+        model: :recipe,
+        icon: 'menu-up'
+      },
+      {
+        model: :ingredient,
+        icon: 'box'
+      },
+      {
+        model: nil, # hr
+      },
+      {
+        model: :ingredient_recipe,
+        name: 'Ingredient and Recipe',
+        icon: 'asterisk'
+      },
+      {
+        model: :step,
+        icon: 'bar-chart-steps'
+      },
+      {
+        model: nil, # hr
+      },
+      {
+        model: :category,
+        icon: 'bookmark-fill'
+      },
+      {
+        model: :tag,
+        icon: 'tags'
+      },
+    ]
+  end
+
+  def is_current_page?(model)
+    controller_name == model.to_s.pluralize && action_name != 'index'
+  end
 end
